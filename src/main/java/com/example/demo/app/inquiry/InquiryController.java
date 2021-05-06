9db76c14f4e980499f65cb6dcf16c8ff59632828
ichika-s <iki.miyabi.1ka@gmail.com>
2021-05-06 19:18:35 +0900

    no message

---
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -0,0 +1,41 @@
+package com.example.demo.app.inquiry;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inquiry")
+
+public class InquiryController {
+
+	@GetMapping("/form")
+	public String from(InquiryForm inquiryForm, Model model) {
+		model.addAttribute("title", "Inquiry Form");
+		return "inquiry/form";
+	}
+
+	@PostMapping("/form")
+	public String fromGoBack(InquiryForm inquiryForm, Model model) {
+		model.addAttribute("title", "Inquiry Form");
+		return "inquiry/form";
+	}
+
+	@PostMapping("/confirm")
+	public String confirm(@Validated InquiryForm inquiryForm,
+			BindingResult result,
+			Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Inquiry Form");
+			return "inquiry/form";
+
+		}
+		model.addAttribute("title", "Confirm Page");
+		return "inquiry/confirm";
+
+	}
+}
